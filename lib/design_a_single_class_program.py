b20daec42fd80251bdf46e703bfc4289339a27a5
Tariq <tariqali87b@gmail.com>
2025-12-08 14:48:17 +0000

    function

---
+++ lib/design_a_single_class_program.py
@@ -0,0 +1,14 @@
+class TodoList:
+    def __init__(self):
+        self.tasks = []
+    
+    def add(self, task):
+        self.tasks.append(task)
+
+    def incomplete(self):
+        return self.tasks
+
+    def mark_as_complete(slef, index):
+        if index < 0 or index >= len(self.tasks):
+            raise Exception("no task available to mark as complete")
+        del self.tasks[index]
---
+++ tests/test_design_a_single_class_program.py
@@ -0,0 +1,5 @@
+from lib.design_a_single_class_program import TodoList
+
+import pytest
+
+def test_adds
